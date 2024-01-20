55be155817204df11c12c38546ec585ad8b2c8c2
Drei4 <antipin-lukinf@mail.ru>
2024-01-20 21:41:54 +0300

    DZ1 start

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# Создать базовый шаблон для интернет-магазина,
+# содержащий общие элементы дизайна (шапка, меню, подвал),
+# и дочерние шаблоны для страниц категорий товаров
+# и отдельных товаров. Например,
+# создать страницы «Одежда», «Обувь» и «Куртка», используя базовый шаблон.
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/main/')
+def main():
+    context = {'title': 'Главная'}
+    return render_template('main.html', **context)
+@app.route('/data/')
+def data():
+    context = {'title': 'База статей'}
+    return render_template('data.html', **context)
